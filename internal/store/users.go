0c891ea0c19d06d9fc4446f8c0adb829327cda26
na1tto <eduardo3245.ss@gmail.com>
2025-10-25 22:23:48 -0300

    feat: User and Post create methods done

--- internal/store/users.go
+++ internal/store/users.go
@@ -5,10 +5,37 @@ import (
 	"database/sql"
 )
 
-type UsersStore struct {
+type User struct {
+	ID        int64  `json:"id"`
+	UserName  string `json:"username"`
+	Emai      string `json:"email"`
+	Password  string `json:"-"` // Password is not marshed
+	CreatedAt string `json:"created_at"`
+}
+
+type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (user_name, email, password)
+		VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.UserName,
+		user.Emai,
+		user.Password,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
