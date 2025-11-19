51e1507e96e8a4530bbb6e9102302983a5574268
na1tto <eduardo3245.ss@gmail.com>
2025-11-19 13:26:27 -0300

    chore: adding payload validation with the validator library

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -3,7 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
+
+var Validate *validator.Validate
+
+func init(){
+	Validate = validator.New(validator.WithRequiredStructEnabled())
+}
 
 func writeJson(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
@@ -14,10 +22,10 @@ func writeJson(w http.ResponseWriter, status int, data any) error {
 func readJson(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1_048_578 // maximum of ONE megabyte for the request
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-	
+
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
-	
+
 	return decoder.Decode(data)
 }
 
@@ -26,6 +34,6 @@ func writeJsonError(w http.ResponseWriter, status int, message string) error {
 	type envelope struct {
 		Error string `json:"error"`
 	}
-	
+
 	return writeJson(w, status, &envelope{Error: message})
 }
