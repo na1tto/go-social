13bb5673e386f36c58b33700a0bb7ec36a2c424c
GitHub Action <action@github.com>
2026-08-12 00:02:04 +0000

    Update version to 1.2.2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,7 +17,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const version = "1.2.1"
+const version = "1.2.2"
 
 //	@title			Go-Social API
 //	@description	This is my swagger documentation for the Go-Social Project!
