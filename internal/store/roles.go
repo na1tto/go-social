6a300b0d2bb57619343d39adbd96df5aa238e099
Luiz Eduardo Pereira Sousa <133920450+na1tto@users.noreply.github.com>
2026-08-12 08:57:28 -0300

    fix: fix typo in Level field in roles.go

--- internal/store/roles.go
+++ internal/store/roles.go
@@ -8,7 +8,7 @@ import (
 type Role struct {
 	ID          int64  `json:"id"`
 	Name        string `json:"name"`
-	Level       int64  `json:"leve"`
+	Level       int64  `json:"level"`
 	Description string `json:"description"`
 }
 
